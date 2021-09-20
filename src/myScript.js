1490591e92b4dcc2d94836a65891bb0155055a9d
SeungHwan-Lee <sh981013s@gmail.com>
2021-09-20 21:47:24 +0900

    .

---
+++ src/myScript.js
@@ -0,0 +1,35 @@
+// 테스트 시작 누르면 얼굴 인식 페이지로 이동
+let contMain = document.querySelector(".cont-main");
+let testStartBtn = document.querySelector(".btn-test-start");
+let cont = document.querySelector(".cont");
+let cover = document.querySelector(".cover");
+
+testStartBtn.addEventListener("click", function(){
+    contMain.style.display = "none";
+    cover.style.display = "block";
+    cont.style.display = "inline-block";
+});
+
+
+const imgSlideItems = document.querySelectorAll(".img-slide-item");
+
+const firstItem = imgSlideItems[0];
+
+function imageSlide() {
+    const currentItem = document.querySelector(".img-slide-item.active");
+
+    if(currentItem) {
+        currentItem.classList.remove("active");
+        let next = currentItem.nextSibling;
+
+        if(next) {
+            next.classList.add("active");
+        } else {
+            firstItem.classList.add("active");
+        }
+    } else {
+        firstItem.classList.add("active");
+    }
+}
+
+setInterval(imageSlide, 200);
